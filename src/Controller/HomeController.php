f639698bf36c3e38c8d21e11e8c00fce321b1b27
Siwar Taktak <siwar.taktak@esprit.tn>
2024-03-23 03:41:45 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    
+    #[Route('/about', name: 'app_about_home')]
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/contact_us', name: 'app_contact_us')]
+    public function contact_us(): Response
+    {
+        return $this->render('home/contact_us.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+}
